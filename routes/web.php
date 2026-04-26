b7f52cb0921d3c1c0910b8e66758796d8733c52f
Maryam Inriyanti <maryam24@mhs.unitama.ac.id>
2026-04-26 14:55:35 +0800

    feat: penyiapan blade component untuk layout utama

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/smartphone', function () {
+    return view('smartphone.index', ['title' => 'Smartphone']);
+});
+
+Route::get('/smartphone/create', function () {
+    return view('smartphone.create', ['title' => 'Create Smartphone']);
+});
